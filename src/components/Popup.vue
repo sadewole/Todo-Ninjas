f8a1e8bf3f28145b6d270ee2ad582e90e4e97252
Samuel Adewole <samueladewole15@gmail.com>
2020-01-29 23:20:07 +0100

    date picker

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -11,6 +11,19 @@
         <v-form class="px-3">
           <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder" />
           <v-textarea label="information" v-model="content" prepend-icon="mdi-pencil" />
+
+          <v-menu>
+            <template v-slot:activator="{on}">
+              <v-text-field
+                v-on="on"
+                label="Due date"
+                :value="formattedDate"
+                prepend-icon="mdi-calendar"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="due"></v-date-picker>
+          </v-menu>
+          <v-btn text class="success mt-3 mx-0" @click="submit">Add projects</v-btn>
         </v-form>
       </v-card-text>
     </v-card>
@@ -18,13 +31,25 @@
 </template>
 
 <script>
+import format from "date-fns/format";
 export default {
   data() {
     return {
       dialog: false,
       title: "",
-      content: ""
+      content: "",
+      due: null
     };
+  },
+  methods: {
+    submit() {
+      //   console.log(this.title);
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(this.due, "Do MMM YYYY") : "";
+    }
   }
 };
 </script>

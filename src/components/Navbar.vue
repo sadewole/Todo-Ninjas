6f0cdc738cb9692a6cf378558cf439c36161e344
Samuel Adewole <samueladewole15@gmail.com>
2020-01-29 11:04:21 +0100

    created form

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,37 +1,57 @@
 <template>
   <nav>
     <v-app-bar app flat>
-      <v-app-bar-nav-icon
-        class="grey--text"
-        @click="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span class="font-weight-bold">Ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+
+      <!-- dropdown menu -->
+      <v-menu offset-y>
+        <template v-slot:activator="{on}">
+          <v-btn text color="grey" v-on="on">
+            <v-icon left>mdi-chevron-down</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-title>{{link.text}}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn text color="grey">
         <span>Sign Out</span>
         <v-icon right>mdi-exit-to-app</v-icon>
       </v-btn>
     </v-app-bar>
 
+    <!-- navigation / side-nav -->
+
     <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="/avatar-1.png" />
+          </v-avatar>
+          <p class="white--text mt-1">The Net Ninja</p>
+        </v-flex>
+        <v-flex class="mt-4 mb-3">
+          <Popup />
+        </v-flex>
+      </v-layout>
+
       <v-list>
         <v-list-item-group>
-          <v-list-item
-            v-for="link in links"
-            :key="link.text"
-            router
-            :to="link.route"
-          >
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
             <v-list-item-icon>
               <v-icon class="white--text">{{ link.icon }}</v-icon>
             </v-list-item-icon>
             <v-list-item-content>
-              <v-list-item-title class="white--text">
-                {{ link.text }}
-              </v-list-item-title>
+              <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
@@ -41,14 +61,16 @@
 </template>
 
 <script>
+import Popup from "./Popup";
 export default {
+  components: { Popup },
   data() {
     return {
       drawer: false,
       links: [
-        { icon: 'mdi-widgets', text: 'Dashboard', route: '/' },
-        { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
-        { icon: 'mdi-account', text: 'Team', route: '/team' }
+        { icon: "mdi-widgets", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-account", text: "Team", route: "/team" }
       ]
     };
   }
---
+++ src/components/Popup.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog v-model="dialog" max-width="600">
+    <template v-slot:activator="{on}">
+      <v-btn text v-on="on" class="success">Add a New Project</v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <h2>Add a New Project</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder" />
+          <v-textarea label="information" v-model="content" prepend-icon="mdi-pencil" />
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      title: "",
+      content: ""
+    };
+  }
+};
+</script>
